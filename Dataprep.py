270dcf82d635e46af08ae079fb40dccc02e0bb59
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-22 12:07:36 -0400

    work
    Former-commit-id: 965150fcffdcee91abb3e6b6c2994a1bae283f1a

--- Dataprep.py
+++ Dataprep.py
@@ -184,7 +184,7 @@ def main():
                 [
                     executor.submit(
                         sample_video(
-                            dataset.loc[0, "file"],
+                            dataset.iloc[0, 0],
                             dataset,
                             number_of_samples,
                             args.frames_per_sample,
