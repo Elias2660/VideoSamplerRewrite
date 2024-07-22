160c1d5ca9149d53346ebbcef2ca25db3fb3fe10
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-22 13:11:31 -0400

    status_update
    Former-commit-id: 49464f35f3fd0b961b1b0895a366b78e0151e459

--- SamplerFunctions.py
+++ SamplerFunctions.py
@@ -10,7 +10,6 @@ import os
 import re
 
 
-
 def sample_video(
     video: str,
     dataframe: pd.DataFrame,
@@ -18,7 +17,7 @@ def sample_video(
     frames_per_sample: int,
     normalize: bool,
     out_channels: int,
-    sample_span:int=1,
+    sample_span: int = 1,
 ):
     start_time = time.time()
     cap = None
@@ -28,50 +27,49 @@ def sample_video(
         -return samples given the interval given
         """
         dataframe = dataframe.copy(deep=True)
-        dataframe["target_samples"] = ''
+        dataframe["target_samples"] = ""
         dataframe["target_samples"] = dataframe["target_samples"].apply(list)
-        
+
         logging.info(f"Dataframe for {video} about to be prepared (0)")
-        
+
         for index, row in dataframe.iterrows():
             begin_frame = row.iloc[2]
             end_frame = row.iloc[3]
             width, height = getVideoInfo(video)
-            available_samples = (end_frame - (sample_span - 1) - begin_frame) // sample_span
+            available_samples = (
+                end_frame - (sample_span - 1) - begin_frame
+            ) // sample_span
             num_samples = min(available_samples, number_of_samples_max)
-            
-            
+
             target_samples = [
                 (begin_frame) + x * sample_span
                 for x in sorted(
                     random.sample(population=range(available_samples), k=num_samples)
                 )
             ]
-            
+
             logging.debug(f"Target samples for {video}: {target_samples}")
             logging.debug(index)
             logging.debug(dataframe.loc[index, "target_samples"])
             dataframe.at[index, "target_samples"] = target_samples
-            
-            
+
         logging.info(f"Dataframe for {video} about to be prepared(1)")
-            
-        dataframe['samples'] = ''
-        dataframe['samples'] = dataframe['samples'].apply(list)
-        dataframe['counts'] = ''
-        dataframe['counts'] = dataframe['counts'].apply(list)        
-        dataframe['partial_sample'] = ''
-        dataframe['partial_sample'] = dataframe['partial_sample'].apply(list)
-        dataframe["samples_recorded"] = False 
+
+        dataframe["samples"] = ""
+        dataframe["samples"] = dataframe["samples"].apply(list)
+        dataframe["counts"] = ""
+        dataframe["counts"] = dataframe["counts"].apply(list)
+        dataframe["partial_sample"] = ""
+        dataframe["partial_sample"] = dataframe["partial_sample"].apply(list)
+        dataframe["samples_recorded"] = False
         dataframe["frame_of_sample"] = 0
-        
-        
+
         logging.info(f"Capture to {video} about to be established")
         cap = cv2.VideoCapture(video)
         if not cap.isOpened():
             logging.error(f"Failed to open video {video}")
             return
-        
+
         while True:
             ret, frame = cap.read()
             if not ret:
@@ -79,40 +77,42 @@ def sample_video(
 
             count += 1
             if count % 10000 == 0:
-                logging.info(f"Frame {count} read from video {video}")  
-                    
+                logging.info(f"Frame {count} read from video {video}")
+
             for index, row in dataframe.iterrows():
                 if count in row["target_samples"]:
-                    logging.debug(f"Frame {count} just triggered the samples_recorded variable")
+                    logging.debug(
+                        f"Frame {count} just triggered the samples_recorded variable"
+                    )
                     dataframe.at[index, "samples_recorded"] = True
                     dataframe.at[index, "frame_of_sample"] = 0
                     dataframe.at[index, "partial_sample"] = []
-                    
+
                 if row["samples_recorded"]:
                     dataframe.at[index, "frame_of_sample"] += 1
                     if normalize:
                         frame = cv2.normalize(
                             frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX
                         )
-                    
+
                     if out_channels == 1:
                         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                         frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-                        
+
                     contrast = 1.9  # Simple contrast control [1.0-3.0]
                     brightness = 10  # Simple brightness control [0-100]
                     frame = cv2.convertScaleAbs(frame, alpha=contrast, beta=brightness)
-                    
+
                 if out_channels == 1:
                     logging.debug(f"Converting frame {count} to greyscale")
                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    
+
                 np_frame = np.array(frame)
                 in_frame = torch.tensor(
                     data=np_frame,
                     dtype=torch.uint8,
                 ).reshape([1, height, width, out_channels])
-                
+
                 in_frame = in_frame[:, :height, :width, :]
 
                 in_frame = in_frame.permute(0, 3, 1, 2).to(dtype=torch.float)
@@ -125,24 +125,26 @@ def sample_video(
                 # read one sample as an image
 
             if row["frame_of_sample"] == frames_per_sample:
-                directory_name = row.loc['data_file'].replace('.csv', '') + "_samplestemporary"
+                directory_name = (
+                    row.loc["data_file"].replace(".csv", "") + "_samplestemporary"
+                )
                 s_c = "-".join([str(x) for x in row["counts"]])
                 d_name = row.iloc[1]
                 if row["frames_per_sample"] == 1:
                     # logging.debug(f"Saving partial sample {dataframe.loc[index, "partial_sample"][0]}")
                     t = dataframe.loc[index, "partial_sample"][0]
-                    
-                    # join the counts list with "_" 
+
+                    # join the counts list with "_"
                     # then encode in frame
                     pt_name = f"{directory_name}/{row.loc['data_file']}_{s_c}_{d_name}_{count}.pt"
                     torch.save(t, pt_name)
                     # dataframe.at[index, samples].append([pt_name, video, counts, row.iloc[1]])
 
                 else:
-             # The line `logging.debug(f"Appending partial sample
-             # {torch.cat(dataframe.loc["partial_sample"][0])}")` is logging a debug message. It is
-             # displaying information about the process of appending a partial sample to the
-             # dataframe.
+                    # The line `logging.debug(f"Appending partial sample
+                    # {torch.cat(dataframe.loc["partial_sample"][0])}")` is logging a debug message. It is
+                    # displaying information about the process of appending a partial sample to the
+                    # dataframe.
                     # logging.debug(
                     #     f"Appending partial sample {torch.cat(dataframe.loc["partial_sample"][0])}"
                     # )
@@ -154,12 +156,10 @@ def sample_video(
                 dataframe.at[index, "counts"] = []
                 dataframe.at[index, "partial_sample"] = []
                 dataframe.at[index, "samples_recorded"] = False
-                
-        logging.info(
-            f"Capture to {video} has been released, writing samples"
-        )
+
+        logging.info(f"Capture to {video} has been released, writing samples")
         end_time = time.time()
-        logging.info("Time taken to sample video: " + str(end_time - start_time))        
+        logging.info("Time taken to sample video: " + str(end_time - start_time))
 
     except Exception as e:
         logging.error(f"Error sampling video {video}: {e}")
@@ -171,6 +171,7 @@ def sample_video(
         cv2.destroyAllWindows()
         logging.info(f"Released video capture for {video}")
     return
+
 
 def getVideoInfo(video: str):
     """
@@ -191,6 +192,5 @@ def getVideoInfo(video: str):
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     finally:
         cap.release()
-
 
     return width, height
