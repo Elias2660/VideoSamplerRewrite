6eb2e6a913bbf6d6a50f6ab37a70b20260de711d
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-22 13:10:01 -0400

    status_update

--- SamplerFunctions.py
+++ SamplerFunctions.py
@@ -49,6 +49,7 @@ def sample_video(
             ]
             
             logging.debug(f"Target samples for {video}: {target_samples}")
+            print(index)
             print(dataframe.at[index, "target_samples"])
             dataframe.at[index, "target_samples"] = target_samples
             
