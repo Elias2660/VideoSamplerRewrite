ea508475dc27b1b29097fa9137b99a08d4fd60ac
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-22 15:07:50 -0400

    update_sh
    Former-commit-id: ec397c57afc37aba1bc8bfd46bbc49bb0ce838de

--- RunDataPrep.sh
+++ RunDataPrep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to prepare the data for the model training
-python VideoSamplerRewrite/Dataprep.py --max-workers 40 >> dataprep.log 2>&1
+python VideoSamplerRewrite/Dataprep.py --max-workers 15 >> dataprep.log 2>&1
