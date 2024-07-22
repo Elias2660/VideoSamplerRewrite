cb1873102382036cf49d3f79ca92bfa96dafed3e
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-21 20:22:07 -0400

    work

--- RunDataPrep.sh
+++ RunDataPrep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to prepare the data for the model training
-python DataPrep.py --max-workers 40 >> DataPrep.log 2>&1
+python Dataprep.py --max-workers 40 >> dataprep.log 2>&1
