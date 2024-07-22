e74c612b57723e369fb9d6eee3b9eff2caf42cd0
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-22 13:19:22 -0400

    status_update

--- SamplerFunctions.py
+++ SamplerFunctions.py
@@ -76,7 +76,7 @@ def sample_video(
                 break
 
             count += 1
-            logging.info(f"Frame {count} read from video {video}")
+            logging.debug(f"Frame {count} read from video {video}")
             if count % 10000 == 0:
                 logging.info(f"Frame {count} read from video {video}")
 
