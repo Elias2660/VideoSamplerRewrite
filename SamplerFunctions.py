5942921aa6bcf53c6991c20709db0481f41dcc87
Restyled.io <commits@restyled.io>
2025-04-06 20:34:37 +0000

    Restyled by pyment

--- SamplerFunctions.py
+++ SamplerFunctions.py
@@ -96,6 +96,20 @@ def sample_video(
     :param crop: bool:  (Default value = False)
     :param max_batch_size: int:  (Default value = 50)
     :param max_threads_pic_saving: int:  (Default value = 10)
+    :param video: str: 
+    :param old_df: pd.DataFrame: 
+    :param number_of_samples_max: int: 
+    :param frames_per_sample: int: 
+    :param normalize: bool: 
+    :param out_channels: int: 
+    :param sample_span: int: 
+    :param out_height: int:  (Default value = None)
+    :param out_width: int:  (Default value = None)
+    :param x_offset: int:  (Default value = 0)
+    :param y_offset: int:  (Default value = 0)
+    :param crop: bool:  (Default value = False)
+    :param max_batch_size: int:  (Default value = 50)
+    :param max_threads_pic_saving: int:  (Default value = 10)
     :returns: None
 
     """
@@ -290,8 +304,8 @@ def save_sample(batch):
     :param frames_per_sample: int
     :param count: int
     :param spc: int
-    :param Raises:
-    :param batch:
+    :param Raises: param batch:
+    :param batch: 
     :returns: None
 
     """
@@ -375,6 +389,16 @@ def apply_video_transformations(
     :param y_offset: int:  (Default value = 0)
     :param out_width: int:  (Default value = 400)
     :param out_height: int:  (Default value = 400)
+    :param count: int: 
+    :param normalize: bool: 
+    :param out_channels: int: 
+    :param height: int: 
+    :param width: int: 
+    :param crop: bool:  (Default value = False)
+    :param x_offset: int:  (Default value = 0)
+    :param y_offset: int:  (Default value = 0)
+    :param out_width: int:  (Default value = 400)
+    :param out_height: int:  (Default value = 400)
     :returns: The transformed video frame as a tensor.
     :rtype: torch.Tensor
 
@@ -421,6 +445,7 @@ def getVideoInfo(video: str):
 
     :param video: str
     :param video: str:
+    :param video: str: 
     :returns: tuple: A tuple containing the width and height of the video.
 
     """
